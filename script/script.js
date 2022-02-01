0af3de718e003ac7180db87c15d591895ea077f0
Michael Calandriello <michaelcole5950@gmail.com>
2022-01-31 21:57:04 -0600

    third commit;   working product completed

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container')
 const slider = document.querySelector('#myRange').value
-// document.container = createGrid()
-// const squares = Array.from(document.querySelectorAll('.squares'));
-// squares.forEach(square => square.addEventListener('mouseover', addFill))
+document.container = createGrid()
 const btnCreate = document.querySelector('.btn-create').onclick = createNewGrid
 const btnClear = document.querySelector('.btn-clear').onclick = clearGrid
 
-function createGrid () {
+function createGrid() {
+    const slider = document.querySelector('#myRange').value
+    const container = document.querySelector('.container')
+    container.setAttribute('style','grid-template-columns: repeat(' + slider + ',' + (100 / slider) + '%)');
     for (let i = 0; i < slider * slider; i++) {
         const grid = document.createElement('div')
         grid.className = 'squares';
@@ -21,7 +22,6 @@ function addFill(e) {
     e.target.classList.add('fill');
   }
 
-
 function createNewGrid(e) {
     const grid = Array.from(document.querySelectorAll('.squares'));
         for (const squares of grid) {
@@ -33,9 +33,10 @@ function createNewGrid(e) {
 function clearGrid(e) {
     const fill = Array.from(document.querySelectorAll('.fill'));
         for (const squares of fill) {
-            squares.classList.remove('fill')
+            squares.classList.remove('fill');
         }
 }
+
 
 
 
